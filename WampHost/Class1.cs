58e3c13b347c7bb58161c040e233bc13855af53f
chenshuangyu <695166116@qq.com>
2017-06-11 19:11:22 +0800

    init

---
+++ WampHost/Class1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Reactive.Linq;
+using System.Reactive.Subjects;
+using WampSharp.V2;
+using WampSharp.V2.Realm;
+
+namespace MyNamespace
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            const string serverAddress = "ws://127.0.0.1:8080/ws";
+
+            DefaultWampHost host = new DefaultWampHost(serverAddress);
+
+            host.Open();
+
+            Console.WriteLine("Press enter after a subscriber subscribes to com.myapp.topic1");
+
+            Console.ReadLine();
+
+            IWampHostedRealm realm = host.RealmContainer.GetRealmByName("realm1");
+
+            ISubject<int> subject =
+                realm.Services.GetSubject<int>("com.myapp.topic1");
+
+            int counter = 0;
+
+            IObservable<long> timer =
+                Observable.Timer(TimeSpan.FromMilliseconds(0),
+                                 TimeSpan.FromMilliseconds(1000));
+
+            IDisposable disposable =
+                timer.Subscribe(x =>
+                {
+                    counter++;
+
+                    Console.WriteLine("Publishing to topic 'com.myapp.topic1': " + counter);
+                    try
+                    {
+                        subject.OnNext(counter);
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex);
+                    }
+                });
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ WampHost/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Threading.Tasks;
+using SystemEx;
+using WampSharp.V2;
+using WampSharp.V2.Realm;
+using WampSharp.V2.Rpc;
+
+namespace MyNamespace
+{
+    public interface IArgumentsService
+    {
+        [WampProcedure("com.arguments.ping")]
+        void Ping();
+
+        [WampProcedure("com.arguments.add2")]
+        int Add2(int a, int b);
+
+        [WampProcedure("com.arguments.stars")]
+        string Stars(string nick = "somebody", int stars = 0);
+
+
+        [WampProcedure("com.arguments.orders")]
+        string[] Orders(string product, int limit = 5);
+    }
+
+    public class ArgumentsService : IArgumentsService
+    {
+        public void Ping()
+        {
+        }
+
+        public int Add2(int a, int b)
+        {
+            return a + b;
+        }
+
+        public string Stars(string nick = "somebody", int stars = 0)
+        {
+            return string.Format("{0} starred {1}x", nick, stars);
+        }
+
+        public string[] Orders(string product, int limit = 5)
+        {
+            return new[] { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+        }
+    }
+
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            const string location = "ws://127.0.0.1:8080/";
+
+            using (IWampHost host = new DefaultWampHost(location))
+            {
+                IArgumentsService instance = new ArgumentsService();
+
+                IWampHostedRealm realm = host.RealmContainer.GetRealmByName("realm1");
+
+                Task<IAsyncDisposable> registrationTask = realm.Services.RegisterCallee(instance);
+                // await registrationTask;
+                registrationTask.Wait();
+
+                host.Open();
+
+                Console.WriteLine("Server is running on " + location);
+                Console.ReadLine();
+            }
+        }
+    }
+}
